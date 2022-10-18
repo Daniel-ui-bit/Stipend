cfb825ce3e8fea87b850eed3b1112ee3cb78fce2
Daniel-ui-bit <114742565+Daniel-ui-bit@users.noreply.github.com>
2022-10-18 20:46:20 +0200

    Leggere modifiche Css/Html
    Aggiunto tag Hr

--- Sito/Index.php
+++ Sito/Index.php
@@ -13,74 +13,93 @@
                 <div class="rec_Sfondo">
             
                     <div class="row">
-                        <div class="col-6">
+                        <div class="col-12">
                             <h1 class="title"> Quanto sarai povero </h1>
                         </div>
                     </div>
-
+                    
+                    
                     <div class="row">
-                        <div class="col-1">
+                        <div class="col-2">
                             <p class="Paraghrap"><b>Giorni</b></p> 
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-8">
                             <input type="text" id="DurataLavoro" name="DurataLavoro" placeholder="Quanti giorni durerà il lavoro?" class="Text-box">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-2">
+                        <div class="col-12">
+                            <hr class="Linea">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4">
                             <p class="Campo_OBG">Campo obbligatorio</p>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-2">
+                        <div class="col-4">
                             <p class="Paraghrap">Ore giornaliere di lavoro</p>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-8">
                             <input type="text" id="QuantoLavori" name="QuantoLavori" placeholder="Quanto lavori al giorno?" class="Text-box">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-2">
+                        <div class="col-12">
+                            <hr class="Linea">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4">
                             <p class="Campo_OBG">Campo obbligatorio</p>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-2">
+                        <div class="col-4">
                             <p class="Paraghrap">Compenso orario</p>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-8">
                             <input type="text" id="PagamentoOra" name="PagamentoOra" placeholder="Quanto verrai pagato all’ora?" class="Text-box">
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-2">
+                        <div class="col-12">
+                            <hr class="Linea">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-4">
                             <p class="Campo_OBG">Campo obbligatorio</p>
                         </div>
                     </div>
                 
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-8">
                             <input type="submit" name="submit" value="Calcola" class="Button">
                         </div>
                     </div>
                     
                     <div class="row">
-                        <div class="col-4">
-                            <p class="Result">Guadagnerai <b><span id="PagamentoLavoro"></span> €</b> lavorando <span id="DurataDelLavoro"></span> giorni al compenso di <span id="PagamentoPerOra"></span> €/h</p>
+                        <div class="col-8">
+                            <p class="Result">Guadagnerai <b><span id="PagamentoLavoro" class="Result"></span> €</b> lavorando <span id="DurataDelLavoro" class="Result"></span> giorni al compenso di <span id="PagamentoPerOra" class="Result"></span> €/h</p>
                         </div>
                     </div>
                     
@@ -94,14 +113,25 @@
 <?php
 if (isset($_POST["submit"])){
         
-   
+
         $DurataLavoro = $_POST["DurataLavoro"];
         $LavoroPerGiorno = $_POST["QuantoLavori"];
         $PagamentoOra = $_POST["PagamentoOra"];
-
-        $PagamentoLavoro = ($LavoroPerGiorno*$PagamentoOra)*$DurataLavoro;
-
-        echo "<script> Stampa($PagamentoLavoro, $DurataLavoro, $PagamentoOra); </script>";
+        if($DurataLavoro != null && $LavoroPerGiorno != null && $PagamentoOra != null ){
+            if( $DurataLavoro>=0 && $LavoroPerGiorno>=0 && $PagamentoOra>=0 )
+            {
+                $PagamentoLavoro = ($LavoroPerGiorno*$PagamentoOra)*$DurataLavoro;
+                echo "<script> Stampa($PagamentoLavoro, $DurataLavoro, $PagamentoOra); </script>";  
+            }
+            else
+            {
+                echo "<script> alert('Attenzione! Inserire un numero maggiore di 0'); </script>";  
+            }
+        }
+        else
+        {
+            echo "<script> alert('Attenzione! Inserire un numero'); </script>";  
+        }
         
     }
 ?>
